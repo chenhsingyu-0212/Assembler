b68d35341247603108af7d9fe19ca1f343aea504
ChienI Kao <alen911018@gmail.com>
2023-05-11 14:53:30 +0800

    init

---
+++ src/C++/SICXE/assembler.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define OPTAB "opcode_table.txt"
+#define SYMTAB "symble_table.txt"
+#define INTERMEDIATE_FILE "intermediate.txt"
+#define SOURCE_FILE "source.txt"
+#define OBJ_FILE "object.txt"
+
+using namespace std;
+
+FILE *fsrc;
+
+void pass1();
+void pass2();
+
+int main () {
+    cout << "=== SIC/XE assembler ===\n\n";
+    cout << "==== pass 1 ====\n\n";
+
+    fsrc = fopen(SOURCE_FILE, "r");
+}
+
+void pass1() {
+
+}
+
+void pass2() {
+
+}
