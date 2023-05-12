ce3254e5f11decfcc83cca01da6762e4289931af
HsingYu <cindychen20030212@gmail.com>
2023-05-12 13:40:31 +0800

    feat: deal with literal feature
    when assembler read LTORG and END add curr literal table in line.

---
+++ src/Java/LTORG.java
@@ -0,0 +1,37 @@
+package src.Java;
+
+import java.util.ArrayList;
+
+public class LTORG {
+    ArrayList<String[]> res;
+
+    public LTORG(){
+        res = new ArrayList<>();
+    }
+
+    public void process(ArrayList<String[]> rawDatas){
+        String[] ltorg = new String[3];
+        for(String[] data : rawDatas){
+            if(data.length >= 2){
+                if(data[1].equals("LTORG") || data[1].equals("END")){
+                    res.add(data);
+                    res.add(ltorg);
+                    ltorg = new String[3];
+                    continue;
+                }else if(data.length >= 3){
+                    if(data[2].charAt(0) == '='){
+                        ltorg[0] = "*";
+                        ltorg[1] = data[2];
+                        ltorg[2] = "";
+                    }
+                }
+            }
+
+            res.add(data);
+        }
+    }
+
+    public ArrayList<String[]> getData(){
+        return res;
+    }
+}
