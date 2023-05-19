ae1428cef2bcfdcca2ff00b50076eb4c763e25d8
JHTNT <jerry1106666@gmail.com>
2023-05-20 00:15:22 +0800

    refactor: print error by `sys.exit()`

--- src/Python/main.py
+++ src/Python/main.py
@@ -1,17 +1,15 @@
-import sys
 import os
+import sys
 
 from core.assembler import Assembler
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please specific one assembly file as argument.")
-        sys.exit()
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
+        sys.exit("Error: should give exactly one assembly file as input.")
 
     path = sys.argv[1]
     if os.path.exists(path) is False:
-        print("The file does not exist.")
-        sys.exit()
+        sys.exit("Error: the given file does not exist.")
     filename = path
 
     # remove file extension if exists
