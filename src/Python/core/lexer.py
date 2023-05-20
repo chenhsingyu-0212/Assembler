a8d1b62ec48245edf6d3f5a37028cf9fe5c30085
JHTNT <jerry1106666@gmail.com>
2023-05-20 17:00:53 +0800

    style: annotate class member type at beginning

--- src/Python/core/lexer.py
+++ src/Python/core/lexer.py
@@ -1,3 +1,10 @@
 class RawLine:
-    def __init__(self, line):
+    line: str
+    label: str
+    operator: str
+    operand: str
+    is_comment: bool
+    __slots__ = tuple(__annotations__)
+
+    def __init__(self, line: str):
         self.line = line
--- src/Python/core/section.py
+++ src/Python/core/section.py
@@ -2,11 +2,14 @@ from core.line import Line
 
 
 class Section:
+    name: str
+    lines: list[Line]
+    __slots__ = tuple(__annotations__)
+
     def __init__(self, name: str):
         self.name = name
-        self.line: Line = None
+        self.lines = []
 
     def create_line(self, label: str, operator: str, operand: str):
         l = Line(label, operator, operand)
-        if self.line is None:
-            self.line = l
+        self.lines.append(l)
