8291da35065f704cd5686bd754209e039a5b6779
JHTNT <jerry1106666@gmail.com>
2023-05-26 10:13:28 +0800

    feat: init register in symbol table

--- src/Python/core/table.py
+++ src/Python/core/table.py
@@ -1,9 +1,9 @@
-class SymbolTable():
+class SymbolTable:
     table: dict[str, int]
     __slots__ = tuple(__annotations__)
 
     def __init__(self) -> None:
-        self.table = {}
+        self.table = {"A": 0, "X": 1, "L": 2, "P": 8, "SW": 9, "B": 3, "S": 4, "T": 5, "F": 6}
 
     def __setitem__(self, name: str, addr: int):
         if name not in self.table:
